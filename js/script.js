c9b4b0d437ddc8533274b333b49ba7184098591a
Slava.Gvsh <slava.gvsh@gmail.com>
2024-04-12 18:29:38 +0300

    fix js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
-  $(".nav__link").click(function (event) {
-    $(".sub-menu__list").slideUp("");
-    $(this).next(".sub-menu__list").toggle("");
-    //  $('body').toggleClass('lock');
+  showDrop();
+
+  $(window).resize(function () {
+    showDrop();
+  });
+
+  $(document).click(function (event) {
+    if (!$(event.target).closest(".nav__list").length) {
+      // Если клик произошел не внутри .nav__menu, скрываем все открытые дропдауны
+      $("li.drop-btn").removeClass("active");
+    }
   });
 });
+
+function showDrop() {
+  var screenWidth = $(window).width();
+  console.log(screenWidth);
+  if (screenWidth < 768) {
+    $(".nav__link").click(function (event) {
+      $(".nav__item").not($(this).parent()).removeClass("active");
+      $(this).parent("li.drop-btn").toggleClass("active");
+
+      event.stopPropagation();
+    });
+  } else {
+    $("li.drop-btn").removeClass("active");
+  }
+}
